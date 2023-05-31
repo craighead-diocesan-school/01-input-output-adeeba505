ce1e1b231f407f77c24fa97336b775908f936507
adeeba505 <124209811+adeeba505@users.noreply.github.com>
2023-05-31 23:44:23 +0000

    I just finsihed the welcome bit of coding

--- code/1-welcome.js
+++ code/1-welcome.js
@@ -3,17 +3,27 @@
 // ####################################
 
 function welcome() {
-  let name = 'Mary'
+  let username = prompt("What's your name?")
 
-  alert('We want to know if you like programming!')
+  alert("Hello " + username)
+  alert("We want to know if you like programming!")
 
-  alert('Do you like programming ' + name + '?')
-  let answer = prompt()
+  prompt("Do you like programming " + username + "?")
 
-  alert('Great, you said ' + answer + '!')
+  alert("Great, you said " + answer + "!")
   alert("Let's learn some code today.")
 }
 
 function questionnaire() {
-  // write the code for your questionnaire here
+  let username = prompt("What's your name?")
+
+  alert('Hello ' + username)
+
+  let breakfast = prompt('What did you have for breakfast ' + username + '?')
+
+  alert('Great, you said ' + breakfast + '!')
+
+  let colour = prompt('What is your favourite colour ' + username + '?')
+
+  alert('Mine too!')
 }
